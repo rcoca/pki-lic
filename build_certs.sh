07e66c6b12d89030ca408d42722671fe08d52b76
Razvan Coca <razvan.coca@gmail.com>
2018-03-05 11:45:03 +0200

    okay, a modified CA.pl and a matching build_certs.sh

--- build_certs.sh
+++ build_certs.sh
@@ -4,11 +4,11 @@ echo
 export OPENSSL_CONF=./openssl.cnf
 if [ ! -f LicensingCA/cacert.pem ]; then
     echo $'************************\n*** Generating ROOT CA ***' 
-    /usr/lib/ssl/misc/CA.pl -newca
+    ./CA.pl -newca
 fi
 
 echo -n $'************************\n*** Generating Client Certificate ***\n ' 
 echo -n $'> '
 read
 
-/usr/lib/ssl/misc/CA.pl -newreq-nodes && /usr/lib/ssl/misc/CA.pl -sign && mv newcert.pem client.pem && mv newkey.pem client_key.pem && rm newreq.pem
+./CA.pl -newreq-nodes && ./CA.pl -sign && mv newcert.pem client.pem && mv newkey.pem client_key.pem && rm newreq.pem
